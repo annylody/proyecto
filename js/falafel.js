4aa048389d6cd23be5d4a86211bf123a921aa101
annylody <90717021+annylody@users.noreply.github.com>
2021-11-15 13:38:19 -0300

    Add files via upload

---
+++ js/falafel.js
@@ -0,0 +1,19 @@
+function aviso(){
+    alert( "¿Esta dispuesto a ver el mejor sitio del mundo mundial?")
+  }
+
+
+function cartelito (){
+  alert ("otro cartel")
+}
+
+function cambiarfondo (){ document.body.style.backgroundColor = "black";
+ document.body.style.color = "black";
+}
+
+
+
+
+function creador(){document.getElementById("by").innerHTML = "annylody";
+
+}
